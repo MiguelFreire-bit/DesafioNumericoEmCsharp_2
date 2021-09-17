9daba448cd7254aa69ec7269ebd53fa880cef27b
Miguel Freire <miguelsilvafreire@hotmail.com>
2021-09-17 09:37:37 -0300

    .

---
+++ DesafioFinal2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] valores1 = Console.ReadLine().Split(' ');
+        int a = Int32.Parse(valores1[0]);
+        int b = Int32.Parse(valores1[1]);
+        int c = Int32.Parse(valores1[2]);
+
+        int MAIORAB = (a + b + Math.Abs(a - b) ) / 2;
+
+        int MAIOR = (MAIORAB + c + Math.Abs(MAIORAB - c)) / 2;
+
+        Console.WriteLine($"{MAIOR} eh o maior");
+
+    }
+}
